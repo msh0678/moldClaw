f5e19cfab042b30e18df81865b988d8eb0f86ec9
msh0678 <moonsh0678@gmail.com>
2026-02-27 19:23:02 +0900

    fix: GeneralSettings export 제거 (파일 삭제됨)

--- src/components/settings/index.ts
+++ src/components/settings/index.ts
@@ -2,7 +2,6 @@
 export { default as SettingsPanel } from './SettingsPanel';
 export { default as SettingsSidebar } from './SettingsSidebar';
 export { default as SettingsModal } from './SettingsModal';
-export { default as GeneralSettings } from './GeneralSettings';
 export { default as ModelSettings } from './ModelSettings';
 export { default as MessengerSettings } from './MessengerSettings';
 export { default as SkillsSettings } from './SkillsSettings';
